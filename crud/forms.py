b8e533e72fb134f96dc208633c766648ca2b1cc3
Aleksei Kavko <108331145+Aleks2317@users.noreply.github.com>
2024-08-23 23:11:01 +0300

    add forms

--- crud/forms.py
+++ crud/forms.py
@@ -1,6 +1,19 @@
 from django import forms
 
 
+# class UserForm(forms.Form):
+#     name = forms.CharField(label='Имя',
+#                            initial='undefined',
+#                            help_text='Введите свое имя',
+#                            )
+#     age = forms.IntegerField(label='Ваш вош возраст?',
+#                              initial=18,
+#                              help_text='Введите свой возраст',
+#                              )
+#     filed_order = ['age', 'name']  # меняем порядок следования полей
+
+
 class UserForm(forms.Form):
-    name = forms.CharField(label='Имя')
-    age = forms.IntegerField(label='Ваш вош возраст?')
+    name = forms.CharField(label='Имя', help_text='Введите ваше имя!', min_length=2, max_length=10)
+    age = forms.IntegerField(label='Ваш Возраст', help_text='Введите свой возраст')
+    reklama = forms.BooleanField(label='Согласны получать рекламу?', required=False)
--- crud/views.py
+++ crud/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from crud.forms import UserForm
 
 
+# def index(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')  # получить значение поля Имя
+#         age = request.POST.get('age')  # получить значение поля Возраст
+#         return HttpResponse(f'<h2>Привет, {name}, твой возраст {age}</h2>')
+#     userform = UserForm()
+#     return render(request, 'index.html', {'form': userform})
+
 def index(request):
+    if request.method == 'POST':
+        userform = UserForm(request.POST)
+        if userform.is_valid:
+            name = userform.cleaned_data['name']  # получить очищенные данные, через объект cleaned_data
+            return HttpResponse(f'<h2>Hello, {name}</h2>')
+        return HttpResponse(f'Invalid date')
     userform = UserForm()
     return render(request, 'index.html', {'form': userform})
+
